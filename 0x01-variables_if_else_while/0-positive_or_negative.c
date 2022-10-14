fff47c70c20f11e3fe734913b912e1bd200a0812
MrWais001 <abdulgafarridwan@gmail.com>
2022-10-14 06:59:03 -0700

    hello

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Prints with proper grammar, but the outcome is art
+ * Return: always 0.
+ */
 int main(void)
 {
 
@@ -12,5 +13,5 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	return (0);
+	return 0;
 }
