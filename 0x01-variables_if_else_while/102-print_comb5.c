718bb76ce0a4167a2801ae413fd351b597894a1d
MrWais001 <abdulgafarridwan@gmail.com>
2022-10-16 04:29:38 -0700

    two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main()
+{
+	int i, j;
+
+	for (i=0, j=1; i<100; i++, j++)
+		putchar(i)
+		putchar(j)
+}
