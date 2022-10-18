08298b668ba72af396f39420c03f4c2e39bd46a6
MrWais001 <abdulgafarridwan@gmail.com>
2022-10-18 12:38:30 -0700

    zero

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * mian print character
- * Return: Always 0
- */
+ * *main - Prints character
+ * *
+ * *Return: 0 if exited properly, non-zero otherwise
+ * */
 int main(void)
 {
-	int str = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
-
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar("\n");
-	return 0;
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
